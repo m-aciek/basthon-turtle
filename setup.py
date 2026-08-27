808470f4b5c5958a5ada4956cfcfdb997a0d7d6a
Maciej Olko <maciej.olko@affirm.com>
2026-08-28 00:02:35 +0200

    Bump package version to 0.1.1

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ Implementation
 
 setuptools.setup(
     name="basthon-turtle",
-    version="0.1.0",
+    version="0.1.1",
     author="Maciej Olko",
     author_email="maciej.olko@gmail.com",
     description="A browser-friendly implementation of Python's turtle module.",
